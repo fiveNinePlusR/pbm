2ebb8a1d35e139d89a5a91b39ada626adda35fd4
fiveNinePlusR <fiveNinePlusR@gmail.com>
2024-12-27 17:25:31 -0800

    turn off papertrail in testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -70,6 +70,9 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
+  # speed up tests
+  PaperTrail.enabled = false
+
   unless ENV['TEST_WITH_LOGGING']
     config.logger = Logger.new(nil)
     config.log_level = :fatal
