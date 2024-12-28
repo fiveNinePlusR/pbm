77854736803b8c2bd512d515fa6e5304c400911d
fiveNinePlusR <fiveNinePlusR@gmail.com>
2024-12-27 17:09:52 -0800

    Turn off logging in test environments by default

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -70,4 +70,8 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
+  unless ENV['TEST_WITH_LOGGING']
+    config.logger = Logger.new(nil)
+    config.log_level = :fatal
+  end
 end
